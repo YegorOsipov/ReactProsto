bf439672e824c6f3ab067ee6bfb9ef8908845d8b
Yegor <gogsyan11@gmail.com>
2022-03-04 22:07:55 +0300

    UseEffect AnalogClock.stories.tsx

--- src/stories/AnalogClock.stories.tsx
+++ src/stories/AnalogClock.stories.tsx
@@ -1,25 +1,64 @@
 import React, {useEffect, useState} from "react";
+import styles from "../components/Clock/AnalogClock.module.css";
 
 export default {
     title: "Analog Clock Demo",
 }
 
 export const AnalogClockExample = () => {
-    let [dateTime, setDateTime] = useState(new Date());
+    const [date, setDate] = useState(new Date());
+    const [analog, setAnalog] = useState(false);
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setDateTime(new Date());
+            setDate(new Date());
         }, 1000);
+
         return () => clearInterval(timer);
     }, []);
 
-    const addZero = (num: number) => String(num).length < 2 ? 0 + String(num) : num;
+    const addZero = (num: number) => num < 10 ? "0" + num : num;
+
 
     return (
-        <div>
-            {addZero(dateTime.getHours())}: {addZero(dateTime.getMinutes())} : {addZero(dateTime.getSeconds())}
-        </div>
-    )
+
+        analog ?
+
+            <div>
+                <button onClick={() => {setAnalog(!analog)}}>dynamic clock</button>
+                <div className={styles.clock}>
+                    <div className={styles.hour_hand} style={{transform: `rotateZ(${date.getHours() * 30}deg)`}}></div>
+                    <div className={styles.min_hand} style={{transform: `rotateZ(${date.getMinutes() * 6}deg)`}}></div>
+                    <div className={styles.sec_hand} style={{transform: `rotateZ(${date.getSeconds() * 6}deg)`}}></div>
+
+                    <span className={styles.twelve}>12</span>
+                    <span className={styles.one}>1</span>
+                    <span className={styles.two}>2</span>
+                    <span className={styles.three}>3</span>
+                    <span className={styles.four}>4</span>
+                    <span className={styles.five}>5</span>
+                    <span className={styles.six}>6</span>
+                    <span className={styles.seven}>7</span>
+                    <span className={styles.eight}>8</span>
+                    <span className={styles.nine}>9</span>
+                    <span className={styles.ten}>10</span>
+                    <span className={styles.eleven}>11</span>
+                </div>
+            </div> :
+            <div>
+                <button onClick={() => {setAnalog(!analog)}}>analog clock</button>
+                <div className={styles.dynamic_clock}>
+                <span>
+                    {addZero(date.getHours())} :
+                </span>
+                    <span>
+                    {addZero(date.getMinutes())} :
+                </span>
+                    <span>
+                    {addZero(date.getSeconds())}
+                </span>
+                </div>
+            </div>
+    );
 }
 
